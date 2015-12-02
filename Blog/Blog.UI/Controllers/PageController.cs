0dec6dde1fbc50aab4e285545d67653d8ad891e4
paaaattighe <patrickstighe@outlook.com>
2015-12-02 11:34:09 -0500

    added basic add page controller and addpage view

---
+++ Blog/Blog.UI/Controllers/PageController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Blog.UI.Models;
+
+namespace Blog.UI.Controllers
+{
+    public class PageController : Controller
+    {
+        // GET: Page
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddNewPage()
+        {
+            var newStaticPageVM = new AddStaticPageVM();
+            return View(newStaticPageVM);
+        }
+    }
+}
