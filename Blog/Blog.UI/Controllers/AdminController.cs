a1c0bf827c2111460f06dec8de732bf1a3fc9601
paaaattighe <patrickstighe@outlook.com>
2015-12-03 18:43:48 -0500

    added all posts view and admin view model

--- Blog/Blog.UI/Controllers/AdminController.cs
+++ Blog/Blog.UI/Controllers/AdminController.cs
@@ -3,21 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Blog.BLL;
+using Blog.UI.Models;
 
 namespace Blog.UI.Controllers
 {
     public class AdminController : Controller
     {
+        private BlogOperations _ops;
         // GET: Admin
         public ActionResult Index()
         {
             return View();
         }
 
-        //public ActionResult AngAdmin()
-        //{
-        //    return View();
-        //}
+        public ActionResult AngAdmin()
+        {
+            _ops = new BlogOperations();
+            var vm = new AdminVM();
+            vm.BlogPosts = _ops.GetAllBlogPosts().BlogPosts;
+            
+            
+            return View(vm);
+        }
 
         public ActionResult Posts()
         {
