84611655a92d48458102abc527ca711907b7053e
Dean Choi <deanchoi@gmail.com>
2015-12-03 16:42:48 -0500

    Added Mock Categories script

---
+++ SQL Scripts/MockCategories.sql
@@ -0,0 +1,18 @@
+USE [Blogs]
+GO
+
+INSERT INTO [dbo].[Categories]
+           ([CategoryTitle])
+     VALUES
+           
+		   ('Events'),
+		   ('Expert Advice'),
+		   ('Food & Drink'),
+		   ('News'),
+		   ('Opinions'),
+		   ('Promotions'),
+		   ('"Pull" Requests')
+
+GO
+
+
