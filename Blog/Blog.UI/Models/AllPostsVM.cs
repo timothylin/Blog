2a9cbddfbb74395d72d33e7b6a539ba358d3687f
Dean Choi <deanchoi@gmail.com>
2015-12-08 16:36:09 -0500

    Pagination semi-implemented

---
+++ Blog/Blog.UI/Models/AllPostsVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Blog.Models;
+
+namespace Blog.UI.Models
+{
+    public class AllPostsVM
+    {
+        public List<BlogPost> BlogPosts { get; set; }
+        public int PostCount { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+
+        public AllPostsVM()
+        {
+            BlogPosts = new List<BlogPost>();
+        }
+
+    }
+}
